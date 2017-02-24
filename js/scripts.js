d6f081ccdccff2f83c803c4bada01199920f89aa
Ryan <ryanjonesirl@gmail.com>
2017-02-24 10:50:04 -0800

    seperate the back end logic from the front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,41 @@
+/*
+* Application: Ping-Pong
+* File Name: scripts.js
+* Programmer: Ryan Jones
+* Version: 02/24/2017
+*/
+
 $(document).ready(function() {
   
-  $("#submit").click(function(event) {
-    
-    event.preventDefault();
-    
-    var string = $("#userInput").val();
+  /**** Back End Logic ****/
+  
+  function doSomething(string) {
     var number = parseInt(string);
     var count = 1;
     var value = "";
 
-
     while (count <= number) {
-      if (count % 3 === 0 && count != 15) {
-        value = "ping";
-      } else if (count % 5 === 0 && count != 15) {
-        value = "pong";
-      } else if (count % 15 === 0) {
+      
+      if (count % 15 === 0) {
         value = "ping-pong";
+      } else if (count % 3 === 0) {
+        value = "ping";
+      } else if (count % 5 === 0) {
+        value = "pong";
       } else {
         value = count;
       }
       count+= 1;
       $("#populateUL").append("<li>" + value + "</li>");
     }
+  }
+  
+  /**** Front End Logic ****/
+  $("#submit").click(function(event) {
+    
+    event.preventDefault();
+    
+    doSomething($("#userInput").val());
+    
   });
 });
